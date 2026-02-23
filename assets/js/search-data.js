18553a42f6291105750b56a1dd6efb45be00e748
JUMP-HOWL <gongjh24@mails.tsinghua.edu.cn>
2026-02-23 09:10:27 +0000

    Deploying to gh-pages from @ JUMP-HOWL/MyPages@6571c0f58b836522585eddad30d0b22efe69be3c 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,156 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-home",
+    title: "Home",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/Academic-Pages/";
+    },
+  },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/Academic-Pages/blog/";
+          },
+        },{id: "nav-publications",
+          title: "Publications",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/Academic-Pages/publications/";
+          },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/Academic-Pages/projects/";
+          },
+        },{id: "nav-repositories",
+          title: "repositories",
+          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/Academic-Pages/repositories/";
+          },
+        },{id: "nav-cv",
+          title: "CV",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/Academic-Pages/cv/";
+          },
+        },{id: "nav-teaching",
+          title: "teaching",
+          description: "Course materials, schedules, and resources for classes taught.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/Academic-Pages/teaching/";
+          },
+        },{id: "nav-people",
+          title: "people",
+          description: "members of the lab or group",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/Academic-Pages/people/";
+          },
+        },{id: "dropdown-bookshelf",
+              title: "bookshelf",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/Academic-Pages/books/";
+              },
+            },{id: "dropdown-blog",
+              title: "blog",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/Academic-Pages/blog/";
+              },
+            },{id: "books-the-godfather",
+          title: 'The Godfather',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/Academic-Pages/books/the_godfather/";
+            },},{id: "news-a-simple-inline-announcement",
+          title: 'A simple inline announcement.',
+          description: "",
+          section: "News",},{id: "news-started-my-phd-journey-at-tsinghua-sigs-under-the-supervision-of-prof-wenbo-ding",
+          title: 'Started my PhD journey at Tsinghua SIGS under the supervision of Prof. Wenbo...',
+          description: "",
+          section: "News",},{id: "news-our-paper-on-contactless-triboelectric-sensing-was-published-in-advanced-electronic-materials",
+          title: 'Our paper on contactless triboelectric sensing was published in Advanced Electronic Materials!',
+          description: "",
+          section: "News",},{id: "news-i-will-be-visiting-the-united-kingdom-from-march-to-april-2025-for-an-academic-exchange-program-funded-by-tsinghua-university",
+          title: 'I will be visiting the United Kingdom from March to April 2025 for...',
+          description: "",
+          section: "News",},{id: "news-our-paper-ultratac-was-accepted-to-iros-2025",
+          title: '🎉 Our paper UltraTac was accepted to IROS 2025!',
+          description: "",
+          section: "News",},{id: "news-our-paper-on-magnetic-field-enhanced-soft-robots-was-published-as-a-cover-article-in-science-advances",
+          title: 'Our paper on magnetic field-enhanced soft robots was published as a Cover Article...',
+          description: "",
+          section: "News",},{id: "news-our-paper-moirétac-was-accepted-to-icra-2026",
+          title: '🎉 Our paper MoiréTac was accepted to ICRA 2026!',
+          description: "",
+          section: "News",},{id: "projects-project-1",
+          title: 'project 1',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/Academic-Pages/projects/1_project/";
+            },},{id: "projects-project-2",
+          title: 'project 2',
+          description: "a project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/Academic-Pages/projects/2_project/";
+            },},{id: "projects-project-3-with-very-long-name",
+          title: 'project 3 with very long name',
+          description: "a project that redirects to another website",
+          section: "Projects",handler: () => {
+              window.location.href = "/Academic-Pages/projects/3_project/";
+            },},{id: "projects-project-4",
+          title: 'project 4',
+          description: "another without an image",
+          section: "Projects",handler: () => {
+              window.location.href = "/Academic-Pages/projects/4_project/";
+            },},{id: "projects-project-5",
+          title: 'project 5',
+          description: "a project with a background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/Academic-Pages/projects/5_project/";
+            },},{id: "projects-project-6",
+          title: 'project 6',
+          description: "a project with no image",
+          section: "Projects",handler: () => {
+              window.location.href = "/Academic-Pages/projects/6_project/";
+            },},{id: "projects-project-7",
+          title: 'project 7',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/Academic-Pages/projects/7_project/";
+            },},{id: "projects-project-8",
+          title: 'project 8',
+          description: "an other project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/Academic-Pages/projects/8_project/";
+            },},{id: "projects-project-9",
+          title: 'project 9',
+          description: "another project with an image 🎉",
+          section: "Projects",handler: () => {
+              window.location.href = "/Academic-Pages/projects/9_project/";
+            },},{id: "teachings-data-science-fundamentals",
+          title: 'Data Science Fundamentals',
+          description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
+          section: "Teachings",handler: () => {
+              window.location.href = "/Academic-Pages/teachings/data-science-fundamentals/";
+            },},{id: "teachings-introduction-to-machine-learning",
+          title: 'Introduction to Machine Learning',
+          description: "This course provides an introduction to machine learning concepts, algorithms, and applications. Students will learn about supervised and unsupervised learning, model evaluation, and practical implementations.",
+          section: "Teachings",handler: () => {
+              window.location.href = "/Academic-Pages/teachings/introduction-to-machine-learning/";
+            },},];
